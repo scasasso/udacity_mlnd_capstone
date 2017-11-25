5e940708015098dfd82aefc074aeec3962c59b8e
Stefano Casasso <stefano.casasso@nexiot.ch>
2017-11-25 17:34:05 +0100

    more plots and tidy it up

--- scripts/explore.py
+++ scripts/explore.py
@@ -1,14 +1,16 @@
+import json
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 import utils
 
 if __name__ == '__main__':
 
     # Get DataFrame
-    df = utils.get_data()
-    df['text_length'] = df.apply(lambda x: len(x['text']), axis=1)
-    df['title_length'] = df.apply(lambda x: len(x['title']) if not pd.isnull(x['title']) else -1, axis=1)
+    df = utils.get_data(subset=False)
 
-    # Plot
+    # Plot histograms
     plots = [{'var_name': 'text_length', 'x_range': (0, 10000), 'x_binwidth': 100, 'x_tickwidth': 1000},
              {'var_name': 'title_length', 'x_range': (0, 200), 'x_binwidth': 10, 'x_tickwidth': 20},
              {'var_name': 'participants_count', 'x_range': (0, 20), 'x_binwidth': 1, 'x_tickwidth': 2},
@@ -17,4 +19,37 @@ if __name__ == '__main__':
              {'var_name': 'likes', 'x_range': (0, 20), 'x_binwidth': 1, 'x_tickwidth': 2}]
 
     for pl_specs in plots:
-        plot_var(df, **pl_specs)
+        utils.plot_var(df, **pl_specs)
+
+    # Other plots
+    # Language breakdown
+    ax = df.loc[df['label'] == 1, 'language'].value_counts().plot(kind='bar', alpha=0.75,
+                                                                  legend=False, log='y',
+                                                                  title='Fake news data: language breakdown')
+    plt.ylabel('Number of articles')
+    plt.tight_layout()
+    plt.savefig('fake_languages.pdf')
+    plt.close('all')
+
+    # Word clouds
+    sw_file_path = '../data/stopwords.txt'
+    sw_list = json.load(open(sw_file_path, 'r'))
+    i_row = 56  # Random one
+
+    wcloud_fake = WordCloud(stopwords=sw_list).generate(df.iloc[np.where((~pd.isnull(df['text'])) & (df['label'] == 1))[0][i_row],
+                                                        df.columns.get_loc('text')])
+    plt.imshow(wcloud_fake, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig('wcloud_fake.pdf')
+    plt.close('all')
+
+    wcloud_real = WordCloud(stopwords=sw_list).generate(df.iloc[np.where((~pd.isnull(df['text'])) & (df['label'] == 0))[0][i_row],
+                                                        df.columns.get_loc('text')])
+    plt.imshow(wcloud_real, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig('wcloud_real.pdf')
+    plt.close('all')
+
+
+
+
